5ef6068711bd001f9b43b88c9575b0720ac98217
Amiralles11 <157740574+Amiralles11@users.noreply.github.com>
2024-04-21 19:47:52 +0200

    Ventanat Nueva Partida -graficos- + clase individuo y sus sliders modelados

--- JuegoDeLaVida1/src/main/java/com/example/juegodelavida1/HelloController.java
+++ JuegoDeLaVida1/src/main/java/com/example/juegodelavida1/HelloController.java
@@ -2,38 +2,47 @@ package com.example.juegodelavida1;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void NuevaPartidaButton() {
-        Stage stage = new Stage();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("VentanaNuevaPartida.fxml"));
-        try {
-            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-            stage.setTitle("Nueva Partida");
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+public class HelloController implements Initializable {
+    private Individuo parametrosData = new Individuo(20, 50,20,80 );
+    private IndividuoParametros modeloParaGUICompartido = new IndividuoParametros(parametrosData);
+        @FXML
+        protected void NuevaPartidaButton() {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("VentanaNuevaPartida.fxml"));
+            try {
+                Scene scene = new Scene(fxmlLoader.load(), 320*3, 240*3);
+                stage.setTitle("Establezca parámetros: ");
+                stage.setScene(scene);
+                IndividuoParameterController p = fxmlLoader.getController();
+                p.loadUserData(this.modeloParaGUICompartido); //Carga los datos del modelo en el gui, todas las ventanas comparten el mismo en este caso
+                p.setStage(stage);
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
     @FXML
     protected void CargarPartidaButton() {
         Stage stage = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("VentanaCargarPartida.fxml"));
         try {
-            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            Scene scene = new Scene(fxmlLoader.load(), 320*3, 240*3);
             stage.setTitle("Cargar Partida");
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.print("Inicialización en ejecución del controlador\n");
     }
 }
---
+++ JuegoDeLaVida1/src/main/java/com/example/juegodelavida1/Individuo.java
@@ -0,0 +1,47 @@
+package com.example.juegodelavida1;
+
+public class Individuo {
+    private int vidas;
+    private int porcentajeReproduccion;
+    private int porcentajeClonacion;
+    private int porcentajeTipoAlReproducirse;
+
+    public Individuo(int vidas, int porcentajeReproduccion, int porcentajeClonacion, int porcentajeTipoAlReproducirse) {
+        this.vidas = vidas;
+        this.porcentajeReproduccion = porcentajeReproduccion;
+        this.porcentajeClonacion = porcentajeClonacion;
+        this.porcentajeTipoAlReproducirse = porcentajeTipoAlReproducirse;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public int getPorcentajeReproduccion() {
+        return porcentajeReproduccion;
+    }
+
+    public int getPorcentajeClonacion() {
+        return porcentajeClonacion;
+    }
+
+    public int getPorcentajeTipoAlReproducirse() {
+        return porcentajeTipoAlReproducirse;
+    }
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+    }
+
+    public void setPorcentajeReproduccion(int porcentajeReproduccion) {
+        this.porcentajeReproduccion = porcentajeReproduccion;
+    }
+
+    public void setPorcentajeClonacion(int porcentajeClonacion) {
+        this.porcentajeClonacion = porcentajeClonacion;
+    }
+
+    public void setPorcentajeTipoAlReproducirse(int porcentajeTipoAlReproducirse) {
+        this.porcentajeTipoAlReproducirse = porcentajeTipoAlReproducirse;
+    }
+}
