36db7f642f6bf214f847a95901789355e4843d7c
Amiralles11 <157740574+Amiralles11@users.noreply.github.com>
2024-05-03 13:59:45 +0200

    .

--- JuegoDeLaVida1/src/main/java/com/example/juegodelavida1/Tablero.java
+++ JuegoDeLaVida1/src/main/java/com/example/juegodelavida1/Tablero.java
@@ -1,37 +1,30 @@
 package com.example.juegodelavida1;
 
-import com.example.juegodelavida1.EstructurasDatos.ListaSimple.ListaSimple;
-
 public class Tablero {
-    ListaSimple<Integer> dimensiones = new ListaSimple<>(2);
+    int[] dimensiones = new int[2];
 
-    public Tablero(Integer filas, Integer columnas) {
-        this.dimensiones.add(filas);
-        this.dimensiones.add(columnas);
+    public Tablero(int filas, int columnas) {
+        this.dimensiones[0] = filas;
+        this.dimensiones[1] = columnas;
     }
 
-    public ListaSimple<Integer> getDimensiones() {
+    public int[] getDimensiones() {
         return dimensiones;
     }
 
-    public void setDimensiones(ListaSimple<Integer> dimensiones) {
+    public void setDimensiones(int[] dimensiones) {
         this.dimensiones = dimensiones;
     }
     public int getFilas() {
-        return dimensiones.getElemento(0).getData();
+        return dimensiones[0];
     }
     public int getColumnas() {
-        return dimensiones.getElemento(1).getData();
+        return dimensiones[1];
     }
     public void setFilas(int filas) {
-        dimensiones.getElemento(0).setData(filas);
+        dimensiones[0] = filas;
     }
     public void setColumnas(int columnas) {
-        dimensiones.getElemento(1).setData(columnas);
-    }
-    @Override
-    public String toString() {
-        return "Filas: " + dimensiones.getElemento(0).getData() +
-                "Columnas: " + dimensiones.getElemento(1).getData();
+        dimensiones[1] = columnas;
     }
 }
