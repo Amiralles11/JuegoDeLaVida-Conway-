775a944407e021f49f60fccd2a00475925d2a613
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2024-05-03 12:18:45 +0200

    Importando estructuras de listas simple y enlazada

---
+++ JuegoDeLaVida1/src/main/java/com/example/juegodelavida1/EstructurasDatos/ListaEnlazada/ElementoLE.java
@@ -0,0 +1,27 @@
+package com.example.juegodelavida1.EstructurasDatos.ListaEnlazada;
+
+public class ElementoLE<TipoDato> {
+    private TipoDato data;
+    private ElementoLE<TipoDato> siguiente;
+
+
+    protected void insertarmeEn(ElementoLE<TipoDato> el) {
+        this.siguiente = el.siguiente;
+        el.siguiente = this;
+    }
+
+    public ElementoLE<TipoDato> getSiguiente() {
+        return this.siguiente;
+    }
+
+    protected void setSiguiente(ElementoLE<TipoDato> el) {
+        this.siguiente = el;
+    }
+
+    public TipoDato getData() {
+        return this.data;
+    }
+    public void setData(TipoDato newData) {
+        this.data = newData;
+    }
+}
---
+++ JuegoDeLaVida1/src/main/java/com/example/juegodelavida1/EstructurasDatos/ListaSimple/ListaSimple.java
@@ -0,0 +1,107 @@
+package com.example.juegodelavida1.EstructurasDatos.ListaSimple;
+
+public class ListaSimple<T> {
+    private ElementoLS<T>[] datos;
+    private final int maximo;
+
+    public ListaSimple(int maximo) {
+        this.maximo = maximo;
+        this.datos = new ElementoLS[maximo];
+    }
+
+    public boolean isVacia(){
+        for (int i = 0; i<maximo; i++){
+            if (datos[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void vaciar(){
+        for(int i=0;i<maximo; i++){
+            this.datos[i]=null;
+        }
+    }
+    protected int add(ElementoLS<T> el){
+        int i=0;
+        while (datos[i] != null){
+            i++;
+        }
+        if (i<maximo){
+            datos[i] = el;
+        }
+        return getNumeroElementos();
+    }
+
+    public void add(T o){
+        ElementoLS<T> ele = new ElementoLS<>(o);
+        add(ele);
+    }
+    public void insert(T s, int posicion){
+        ElementoLS<T> e = new ElementoLS<>(s);
+        Integer i = 0;
+        if (datos[posicion] == null){
+            datos[posicion] = e;
+        }else{
+            if(datos[maximo-1] == null){
+                for (int j=0;j<getNumeroElementos();j++) {
+                    datos[getPosicion(getUltimo().getData()) - j] = datos[getPosicion(getUltimo().getData()) + 1 + j];
+                }
+            }
+            datos[posicion] = e;
+        }
+    }
+
+    public int del(int posicion){
+        ElementoLS<T> e = new ElementoLS<>();
+        for(int i=0; posicion + i + 1 < maximo; i++){
+            if (datos[posicion]!=null){
+                datos[posicion+i] = datos[posicion + i + 1];
+            }else{
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    public Integer getPosicion(T el) {
+        for (int i=0; i < maximo; i++){
+            if (datos[i].getData() == el){
+                return i;
+            }
+        }
+        return null;
+    }
+    public ElementoLS<T> getPrimero(){
+        return datos[0];
+    }
+    public ElementoLS<T> getSiguiente(T el){
+        ElementoLS<T> e = new ElementoLS<>(el);
+        int i = 0;
+        while (i < maximo-1) {
+            if (datos[i].getData()==e.getData()) {
+                return datos[i+1];
+            }
+            i++;
+        }
+        return null;
+    }
+    public ElementoLS<T> getElemento(int posicion){
+        if (datos[posicion]!=null){
+            return datos[posicion];
+        }
+        return null;
+    }
+    public int getNumeroElementos(){
+        int num = 0;
+        for (int i=0;i<maximo;i++){
+            if (datos[i]!=null){
+                num++;
+            }
+        }
+        return num;
+    }
+    public ElementoLS<T> getUltimo(){
+        return datos[getNumeroElementos()-1];
+    }
+}
